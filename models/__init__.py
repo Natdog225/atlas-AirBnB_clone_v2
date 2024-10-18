e05b68ce491a0e553b48fe57f7e49e852cf71539
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-10-18 17:22:49 -0500

    Ideally might've fixed it.

--- models/__init__.py
+++ models/__init__.py
@@ -4,12 +4,6 @@ This is the "models" module.
 """
 
 from models.engine.file_storage import FileStorage
-from models.base_model import BaseModel
 
 storage = FileStorage()
-
-# Create a BaseModel instance and save it to create the file.json file
-bm = BaseModel()
-bm.save()
-
 storage.reload()
