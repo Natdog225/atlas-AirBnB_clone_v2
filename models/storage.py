8ffd508443705fd452f7de5080aa9d9dd4511474
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-10-21 11:20:20 -0500

    Fixed an import on storage.py

--- models/storage.py
+++ models/storage.py
@@ -1,5 +1,5 @@
 # models/storage.py
 
-from models.engine.file_storage import FileStorage/file_storage.py import FileStorage
+from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
