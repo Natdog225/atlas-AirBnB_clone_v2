f16739e79895aec708c71ec2dae08a1c5d374d40
Paintballskaguy <Johnstournament@gmail.com>
2024-10-22 17:16:03 +0000

    updated base model on the storage.new(self) line 25

--- models/base_model.py
+++ models/base_model.py
@@ -22,7 +22,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-        storage.new(self.__class__.__name__ + '.' + self.id, self)
+        storage.new(self)
 
 
 
