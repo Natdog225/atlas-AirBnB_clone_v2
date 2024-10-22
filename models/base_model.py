d989dba6b5c39d1047ea6ea1bb83e505a0cc573f
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-10-21 20:06:05 -0500

    Attempting to resolve errors.

--- models/base_model.py
+++ models/base_model.py
@@ -1,41 +1,28 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
-from datetime import datetime, timezone
+from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime
 from models import storage
 
-Base = declarative_base()
-
 class BaseModel:
-    """A base class for all hbnb models"""
-    id = Column(String(60), primary_key=True, nullable=False)
-    created_at = Column(DateTime, nullable=False, default=datetime.now(timezone.utc))
-    updated_at = Column(DateTime, nullable=False, default=datetime.now(timezone.utc))
-
     def __init__(self, *args, **kwargs):
         """Initiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now(timezone.utc)
-            self.updated_at = datetime.now(timezone.utc)
-            #models.storage.new(self)
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+            if hasattr(self, "created_at") and isinstance(self.created_at, str):
+                self.created_at = datetime.strptime(kwargs["created_at"], "%Y-%m-%d %H:%M:%S.%f")
+            if hasattr(self, "updated_at") and isinstance(self.updated_at, str):
+                self.updated_at = datetime.strptime(kwargs["updated_at"], "%Y-%m-%d %H:%M:%S.%f")
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            if not isinstance(kwargs['updated_at'], datetime):
-                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                         '%Y-%m-%dT%H:%M:%S.%f%z')
-            if not isinstance(kwargs['created_at'], datetime):
-                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                         '%Y-%m-%dT%H:%M:%S.%f%z')
-            if '__class__' in kwargs:
-                del kwargs['__class__']
-            self.__dict__.update(kwargs)
-            #print(f"Loaded existing {type(self).__name__} with id {self.id}")
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+        storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the instance"""
