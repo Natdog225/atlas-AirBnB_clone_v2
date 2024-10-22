0e5b1beb2de6dcfc194d552973f8932405919900
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-10-22 15:25:42 -0500

    updated city

--- models/city.py
+++ models/city.py
@@ -7,17 +7,12 @@ from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """
-    City class that inherits from BaseModel
-    Public class attributes:
-        state_id: string - empty string: it will be the State.id
-        name: string - empty string
-    """
     state_id = ""
     name = ""
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        if not kwargs:
+        if 'state_id' not in kwargs:
             self.state_id = ""
+        if 'name' not in kwargs:
             self.name = ""
