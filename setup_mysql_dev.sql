efef9070042ce71f20361082029afdd8e66a030f
Paintballskaguy <Johnstournament@gmail.com>
2024-10-28 17:54:43 +0000

    worked on tests  and pycode and sql files.

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,10 @@
 -- setup_mysql_dev.sql
 -- Script to set up MySQL database and user for the project.
 
+-- command to run this whole script ; mysql -u root -p < setup_mysql_dev.sql 
+-- create the test server.
+-- mysql -u root -p < setup_mysql_test.sql
+
 -- creates the database 'hbnb_dev_db' if it doesn't already exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,16 +2,19 @@
 -- Script to set up MySQL database and user for the project.
 
 -- creates the database 'hbnb_dev_db' if it doesn't already exist
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
 -- Creates the user 'localhost' if it doesn't exist yet.
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
 -- Grant the localhost user all rights to the database
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
 -- Grant select privileges on 'performance_schema' to the user localhost
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+-- Grant permissions on the test server to the main user.
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_dev'@'localhost';
 
 -- Apply the changes
 FLUSH PRIVILEGES;
